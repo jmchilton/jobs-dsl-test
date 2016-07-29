b78982b811cba2f2ec031c38ba9595d75983a397
John Chilton <jmchilton@gmail.com>
2016-07-28 22:21:58 -0400

    Typo.

--- buildjobs.groovy
+++ buildjobs.groovy
@@ -17,7 +17,7 @@ recipePath.eachFile {
     def recipeName = it.name
     def jobName = "${baseJobName}${recipeName}"
     def sout = new StringBuilder(), serr = new StringBuilder()
-    def proc = "cd $ROOT_PATH; make dump-description RECIPE_NAME=${recipeName}".execute()
+    def proc = "cd ${rootPath}; make dump-description RECIPE_NAME=${recipeName}".execute()
     proc.consumeProcessOutput(sout, serr)
     proc.waitForOrKill(1000)
     println "out> $sout err> $serr"
